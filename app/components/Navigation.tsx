4387a30837758173d12ac39244bb7a662eea00e5
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2023-05-25 07:25:34 +0200

    update Navigation.tsx
    Change element function added

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,36 +1,61 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 const links = [
   {
     label: "Inicio",
+    link: true,
   },
   {
     label: "Historia",
+    link: false,
   },
   {
     label: "Productos",
+    link: false,
   },
   {
     label: "Contacto",
+    link: false,
   },
 ];
 
 function Navigation() {
+  const [active, setActive] = useState(links);
 
-    const [active, setActive] = useState<boolean>(true)
+  const handleClick = (label: string) => {
+    const updateLink = active.map((link) => {
+      console.log(label);
+      if (link.label === label) {
+        return { ...link, link: true };
+      } else {
+        return { ...link, link: false };
+      }
+    });
+    setActive(updateLink);
+  };
 
   return (
     <header className="w-[80%] m-auto">
       <div className="hidden md:flex justify-between ">
         <Image src={"/logo.png"} alt="web-logo" width={100} height={100} />
-        <div className="flex fixed top-3 left-1/2 transform -translate-x-1/2 gap-10 bg-[#806D51] h-16 items-center px-8">
-          {links.map(({ label }) => (
-            <Link key={label} href={""}>
-              <li className="list-none text-white font-bold">{label}</li>
+        <div className="flex fixed top-3 left-1/2 transform -translate-x-1/2 gap-3 bg-[#806D51] h-16 items-center p-2 rounded-[50px]">
+          {active.map(({ label, link }) => (
+            <Link key={label} href="">
+              <li
+                onClick={() => handleClick(label)}
+                className={
+                  link
+                    ? "list-none text-white font-bold bg-[#6D310E] p-3 px-10 rounded-[50px]"
+                    : "list-none text-white font-bold p-3 px-10"
+                }
+              >
+                {label}
+              </li>
             </Link>
           ))}
         </div>
